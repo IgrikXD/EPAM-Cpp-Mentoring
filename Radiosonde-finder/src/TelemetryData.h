5cbd3a40bd526be6b79b6c1509b46e4933cf1d46
IgrikXD <igor.nikolaevich.96@gmail.com>
2019-08-12 15:44:04 +0300

    Add TelemetryData interface

---
+++ Radiosonde-finder/src/TelemetryData.h
@@ -0,0 +1,23 @@
+#ifndef TELEMETRYDATA_H
+#define TELEMETRYDATA_H
+
+#include "GpsData.h"
+#include "TimeData.h"
+
+class TelemetryData {
+    private:
+        GpsData m_gps_data;
+        TimeData m_time_data;
+        double m_vertical_speed;
+    public:
+        TelemetryData();
+        TelemetryData(const GpsData gps_data, const TimeData time_data, const double vertical_speed);
+        GpsData getGpsData(void) const;
+        TimeData getTimeData(void) const;
+        double getVerticalSpeed(void) const;
+        void setGpsData(const GpsData gps_data);
+        void setTimeData(const TimeData time_data);
+        void setVerticalSpeed(const double vertical_speed);
+};
+
+#endif
