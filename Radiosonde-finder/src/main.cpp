7052efc58160971418f0c6ad526e02bac4a17c60
IgrikXD <igor.nikolaevich.96@gmail.com>
2019-08-21 13:57:57 +0300

    Add dummy vector for KML file data

--- Radiosonde-finder/src/main.cpp
+++ Radiosonde-finder/src/main.cpp
@@ -1,8 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
 #include "GpsData.h"
 #include "TimeData.h"
 #include "TelemetryData.h"
+#include "tools.h"
 
 int main(int argc, char * argv[]) {
+
+    std::string raw_coordinates {parseKmlFile("R2141624.kml")};
+    std::vector<GpsData> gps_data;
+
+    getGpsData(gps_data, raw_coordinates);
+
+    /* 
+    for (GpsData x : gps_data) {
+
+        std::cout << x.getLatitude() << " " 
+                << x.getLongitude() << " " 
+                << x.getAltitude() << std::endl;
+
+    }
+    */
 
     return 0;
 
